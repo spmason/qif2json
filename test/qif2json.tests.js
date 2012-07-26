4a3290aea75ecc07aef435c0adecb4d643ae13b0
Steve Mason <steve@spmason.com>
2012-07-26 22:22:29 +0100

    Added charset detection

--- test/qif2json.tests.js
+++ test/qif2json.tests.js
@@ -1,3 +1,4 @@
+'use strict';
 var qif2json = require('../lib/qif2json.js');
 
 describe('qif2json', function(){
@@ -32,8 +33,22 @@ PSPRINGFIELD WATER UTILITY\r\n\
         expect(data.transactions[2].payee).toEqual('SPRINGFIELD WATER UTILITY');
     });
 
+    it('Can parse Bank example with single entry', function(){
+        var data = qif2json.parse('!Type:Bank\r\n\
+D03/03/10\r\n\
+T-379.00\r\n\
+PCITY OF SPRINGFIELD\r\n');
+
+        expect(data.type).toEqual('Bank');
+        expect(data.transactions.length).toEqual(1);
+        
+        expect(data.transactions[0].date).toEqual('2010-03-03');
+        expect(data.transactions[0].amount).toEqual(-379);
+        expect(data.transactions[0].payee).toEqual('CITY OF SPRINGFIELD');
+    });
+
     it('errors on invalid type field', function(){
-        var err;
+        var err, data;
         try{
             data = qif2json.parse('!FOO');
         }catch(e){
@@ -44,7 +59,7 @@ PSPRINGFIELD WATER UTILITY\r\n\
     });
 
     it('errors on unknown detail code', function(){
-        var err;
+        var err, data;
         try{
             data = qif2json.parse('!Type:Bank\r\n\
 123\r\n\
@@ -55,5 +70,23 @@ PSPRINGFIELD WATER UTILITY\r\n\
         }
 
         expect(err).toBeDefined();
+    });
+
+    it('can parse a UTF8 file', function(done){
+        qif2json.parseFile(__dirname + '/files/utf8.qif', function(err, data){
+            expect(err).not.toBeDefined();
+            expect(data.transactions[0].payee).toEqual('SOME £$™€ CHARACTERS');
+
+            done();
+        });
+    });
+
+    it('can parse a windows-1252 file', function(done){
+        qif2json.parseFile(__dirname + '/files/windows-1252.qif', function(err, data){
+            expect(err).not.toBeDefined();
+            expect(data.transactions[0].payee).toEqual('SOME £$™€ CHARACTERS');
+
+            done();
+        });
     });
 });
