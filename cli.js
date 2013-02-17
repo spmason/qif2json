4ee4ad567695c8381ac34f99039b5940d8466552
Steve Mason <steve@spmason.com>
2013-02-17 00:48:03 +0000

    cli now supports --transactions param - output only the transactions array

--- cli.js
+++ cli.js
@@ -1,16 +1,37 @@
 #!/usr/bin/env node
 
 var qif2json = require('./lib/qif2json.js'),
-    file = process.argv[2];
+    args = process.argv.slice(2),
+    transactionsOnly,
+    file = [];
 
+args.forEach(function(arg){
+    'use strict';
+    if(arg.indexOf('-') !== 0){
+        file = arg;
+        return;
+    }
+    switch(arg){
+        case '--transactions':
+        case '-t':
+            transactionsOnly = true;
+            break;
+    }
+});
 if(!file){
-    return console.error('Usage: qif2json [file]');
+    console.error('Usage: qif2json [files]');
+    console.error('    --transactions, -t Generate only transactions');
+    return;
 }
 
 qif2json.parseFile(file, function(err, data){
     'use strict';
     if(err){
         return console.error(err.message);
+    }
+
+    if(transactionsOnly){
+        data = data.transactions;
     }
 
     console.log(JSON.stringify(data, null, 4));
