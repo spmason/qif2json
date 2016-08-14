5c88a1342eb5a310a650491d7d7abcbc6f48e246
Steve Mason <smason82@gmail.com>
2016-08-14 21:39:27 +0100

    Added normalTransactions.qif example file + tests

---
+++ test/normalTransactions.tests.js
@@ -0,0 +1,46 @@
+'use strict';
+var qif2json = require('../lib/qif2json.js'),
+    fs = require('fs');
+
+describe('normalTransactions', function(done) {
+    var reader = fs.createReadStream(__dirname + '/files/normalTransactions.qif');
+
+    it ('can parse normalTransactions', function(done) {
+        qif2json.parseStream(reader, {dateFormat: 'us'}, function(err, qifData) {
+            expect(err).toBeUndefined();
+
+            expect(qifData.type).toEqual('Bank');
+            expect(qifData.transactions.length).toEqual(3);
+            expect(qifData.transactions[0].date).toEqual('1994-06-01');
+            expect(qifData.transactions[0].amount).toEqual(-1000);
+            expect(qifData.transactions[0].number).toEqual('1005');
+            expect(qifData.transactions[0].payee).toEqual('Bank Of Mortgage');
+            expect(qifData.transactions[0].category).toEqual('[linda]');
+            expect(qifData.transactions[0].division.length).toEqual(2);
+            expect(qifData.transactions[0].division[0].category).toEqual('[linda]');
+            expect(qifData.transactions[0].division[0].amount).toEqual(-253.64);
+            expect(qifData.transactions[0].division[1].category).toEqual('Mort Int');
+            expect(qifData.transactions[0].division[1].amount).toEqual(-746.36);
+
+            expect(qifData.transactions[1].date).toEqual('1994-06-02');
+            expect(qifData.transactions[1].amount).toEqual(75);
+            expect(qifData.transactions[1].payee).toEqual('Deposit');
+
+            expect(qifData.transactions[2].date).toEqual('1994-06-03');
+            expect(qifData.transactions[2].amount).toEqual(-10);
+            expect(qifData.transactions[2].payee).toEqual('JoBob Biggs');
+            expect(qifData.transactions[2].memo).toEqual('J.B. gets bucks');
+            expect(qifData.transactions[2].category).toEqual('Entertain');
+            expect(qifData.transactions[2].address).toEqual([
+                '1010 Rodeo Dr.',
+                'Waco, Tx',
+                '80505',
+                '',
+                '',
+                ''
+            ]);
+
+            done();
+        });
+    });
+});
