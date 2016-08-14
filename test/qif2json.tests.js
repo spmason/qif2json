d097f62798ee2f1943b17193d0d6d101717439a3
Steve Mason <smason82@gmail.com>
2016-08-14 21:38:35 +0100

    Improved date parsing - added support for us date formats

--- test/qif2json.tests.js
+++ test/qif2json.tests.js
@@ -156,4 +156,40 @@ describe('qif2json', function() {
         expect(data.transactions[0].division[2].amount).toEqual(-6);
     });
 
+    it ('can parse us dates', function() {
+        var data = qif2json.parse(['!Type:Bank',
+            'D10/9/16',
+            'T1',
+            'POpening Balance'].join('\r\n'), {dateFormat: 'us'});
+
+        expect(data.type).toEqual('Bank');
+        expect(data.transactions[0].date).toEqual('2016-10-09');
+        expect(data.transactions[0].amount).toEqual(1);
+        expect(data.transactions[0].payee).toEqual('Opening Balance');
+    });
+
+    it ('can parse dates < 2010', function() {
+        var data = qif2json.parse(['!Type:Bank',
+            'D11/10/9',
+            'T1',
+            'POpening Balance'].join('\r\n'), {dateFormat: 'us'});
+
+        expect(data.type).toEqual('Bank');
+        expect(data.transactions[0].date).toEqual('2009-11-10');
+        expect(data.transactions[0].amount).toEqual(1);
+        expect(data.transactions[0].payee).toEqual('Opening Balance');
+    });
+
+    it ('can parse dates < 2000', function() {
+        var data = qif2json.parse(['!Type:Bank',
+            'D11/10/99',
+            'T1',
+            'POpening Balance'].join('\r\n'), {dateFormat: 'us'});
+
+        expect(data.type).toEqual('Bank');
+        expect(data.transactions[0].date).toEqual('1999-11-10');
+        expect(data.transactions[0].amount).toEqual(1);
+        expect(data.transactions[0].payee).toEqual('Opening Balance');
+    });
+
 });
