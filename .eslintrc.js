7bd3c35296b80ad94674263cec049872886d08af
Steve Mason <smason82@gmail.com>
2019-02-22 23:51:12 +0000

    Replaces jscs and jshint with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    expect: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+  },
+};
--- cli.js
+++ cli.js
@@ -1,39 +1,43 @@
 #!/usr/bin/env node
-'use strict';
-var qif2json = require('./lib/qif2json.js'),
-    args = process.argv.slice(2),
-    transactionsOnly,
-    file;
+/* eslint no-console: "off" */
+const qif2json = require('./lib/qif2json.js');
 
-args.forEach(function(arg) {
-    if (arg.indexOf('-') !== 0) {
-        file = arg;
-        return;
-    }
-    switch (arg) {
-        case '--transactions':
-        case '-t':
-            transactionsOnly = true;
-            break;
-    }
+const args = process.argv.slice(2);
+let transactionsOnly;
+let file;
+
+args.forEach((arg) => {
+  if (arg.indexOf('-') !== 0) {
+    file = arg;
+    return;
+  }
+  switch (arg) {
+    case '--transactions':
+    case '-t':
+      transactionsOnly = true;
+      break;
+    default:
+      break;
+  }
 });
 
 function output(err, data) {
-    if (err) {
-        console.error(err.message);
-        return;
-    }
+  let finalData = data;
+  if (err) {
+    console.error(err.message);
+    return;
+  }
 
-    if (transactionsOnly) {
-        data = data.transactions;
-    }
+  if (transactionsOnly) {
+    finalData = data.transactions;
+  }
 
-    console.log(JSON.stringify(data, null, 4));
+  console.log(JSON.stringify(finalData, null, 4));
 }
 
 if (!file) {
-    qif2json.parseStream(process.stdin, output);
-    process.stdin.resume();
+  qif2json.parseStream(process.stdin, output);
+  process.stdin.resume();
 } else {
-    qif2json.parseFile(file, output);
+  qif2json.parseFile(file, output);
 }
