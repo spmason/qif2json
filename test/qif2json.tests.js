50f764bd73bea8346588c87833a1b9eeff634912
Steve Mason <steve@spmason.com>
2019-06-26 22:51:40 +0100

    Add timestamp parsing for dates

--- test/qif2json.tests.js
+++ test/qif2json.tests.js
@@ -219,4 +219,28 @@ describe('qif2json', () => {
     expect(data.transactions[0].division[2].amount).toEqual(-6);
     expect(data.transactions[0].division[1].description).toEqual('Pork belly');
   });
+
+  it('Can parse timestamp example', () => {
+    const data = qif2json.parse(['!Type:Bank',
+      'D26-06-2019 00:00:00',
+      'T-379.00',
+      'PCITY OF SPRINGFIELD',
+      '^',
+      'D27-06-2019 00:00:00',
+      'T-20.28',
+      'PYOUR LOCAL SUPERMARKET',
+      '^',
+      'D28-06-2019 00:00:00',
+      'T-421.35',
+      'PSPRINGFIELD WATER UTILITY',
+      '^',
+    ].join('\r\n'));
+
+    expect(data.type).toEqual('Bank');
+    expect(data.transactions.length).toEqual(3);
+
+    expect(data.transactions[0].date).toEqual('2019-06-26');
+    expect(data.transactions[0].amount).toEqual(-379);
+    expect(data.transactions[0].payee).toEqual('CITY OF SPRINGFIELD');
+  });
 });
