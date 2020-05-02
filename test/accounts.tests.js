32d0fb13321ec202f0c73122002ee50b826eb332
Daniel Gustaw <gustaw.daniel@gmail.com>
2020-05-02 16:34:33 +0200

    Duplicates of accounts fixed (when accounts have the same name)

--- test/accounts.tests.js
+++ test/accounts.tests.js
@@ -18,4 +18,53 @@ describe('accounts', () => {
       done();
     });
   });
+
+  it('handle with duplicates', () => {
+    const data = qif2json.parse(`!Account
+NAlior GBP
+TBank
+^
+NAlior PLN
+TBank
+^
+!Account
+NAlior PLN
+TBank
+^
+!Type:Bank
+D04/30'16
+PWeb Page
+U750.00
+T750.00
+LIncome:Invoices
+^
+D05/04'16
+PAccounting
+U-668.28
+T-668.28
+LCompany:Accounting
+^
+!Account
+NAlior GBP
+TBank
+^
+!Type:Bank
+D04/30'16
+PNew computer
+U900.00
+T900.00
+LCompany:Devices
+^
+D05/04'16
+PCoffee
+U-855.28
+T-855.28
+LFood:Drink
+^`);
+
+    console.log(data);
+
+    expect(data.transactions.filter((t) => t.account === 'Alior GBP').length).toEqual(2);
+    expect(data.accounts.filter((t) => t.name === 'Alior GBP').length).toEqual(1);
+  });
 });
